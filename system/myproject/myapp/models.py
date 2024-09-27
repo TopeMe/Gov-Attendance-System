72c5b76e8117af08c2eeb69ae1e43d805a22a4f6
leiconcordia <leiconcordia2005@gmail.com>
2024-09-27 22:25:58 +0800

    models

--- system/myproject/myapp/models.py
+++ system/myproject/myapp/models.py
@@ -1,5 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Department Model
+class Department(models.Model):
+    department_id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=255)
+    total_employees = models.IntegerField(default=0)
+    total_present = models.IntegerField(default=0)
+    total_absent = models.IntegerField(default=0)
+    total_late = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.department_name
+
+# Employee Model
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
+    employee_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='employees')
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+# Attendance Record Model
+class AttendanceRecord(models.Model):
+    record_id = models.AutoField(primary_key=True)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='attendance_records')
+    date = models.DateField()
+    arrival_time = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='attendance_records')
+
+    def __str__(self):
+        return f"{self.employee.name} - {self.date}"
+
+# Daily Summary Report Model
+class DailySummaryReport(models.Model):
+    report_id = models.AutoField(primary_key=True)
+    date = models.DateField()
+    total_attended = models.IntegerField(default=0)
+    total_absent = models.IntegerField(default=0)
+    total_late = models.IntegerField(default=0)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='daily_reports')
+
+    def __str__(self):
+        return f"{self.department.department_name} - {self.date}"
